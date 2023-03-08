a40981fe026eded93195188fed567e43bcad342d
Zebreu <sebouel@gmail.com>
2023-03-07 21:50:42 -0500

    Add entrypoint for the bot in the right branch

---
+++ main.py
@@ -0,0 +1,22 @@
+# This example requires the 'message_content' intent.
+import os
+import discord
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello troubleshooter! I am your friendly Computer')
+
+client.run(os.environ['bot_token'])
